eb4c7a4620ce32a0243443220be1b4bcd920161c
Brian Chu <brian.chu@optiver.com.au>
2014-05-31 16:56:42 +1000

    Use termcap for key codes

--- lib/keyboard.sh
+++ lib/keyboard.sh
@@ -1,11 +1,13 @@
 bindkey -v
 
+autoload termcap
+
 bindkey '\e[1~' beginning-of-line
 bindkey '\e[2~' overwrite-mode
 bindkey '\e[3~' delete-char
 bindkey '\e[4~' end-of-line
-bindkey '\e[A'  history-substring-search-up
-bindkey '\e[B'  history-substring-search-down
-bindkey '\e[C'  forward-char
-bindkey '\e[D'  backward-char
+bindkey "${termcap[ku]}" history-substring-search-up
+bindkey "${termcap[kd]}" history-substring-search-down
+bindkey "${termcap[kr]}" forward-char
+bindkey "${termcap[kl]}" backward-char
 bindkey '^q'    push-line-or-edit
